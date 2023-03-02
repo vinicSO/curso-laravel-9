6149a209697a3c81655c58baf78a9d11699edad9
Vinícius S Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-03-02 09:25:54 -0300

    created models, migration and relationships to Comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
 
         return $users;
     }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
 }
---
+++ database/migrations/2023_03_02_121342_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id', 'id')->constrained('users');
+            $table->text('body');
+            $table->boolean('visible')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
